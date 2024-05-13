728afc19112e11b8f45135f22d2cf59f4a216928
jbcrucillo <167048171+jbcrucillo@users.noreply.github.com>
2024-05-13 10:44:16 +0800

    Delete Crucillo_B2_answer directory
    repeat

--- Crucillo_B2_answer/crucillo_b2.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("Exam_table.csv")
-mean_concentration = df.groupby("Stage")["Cry1Ac,ppmDW"].mean().reset_index()
-print(mean_concentration)
-mean_concentration.to_csv("./b2_output1.csv")
